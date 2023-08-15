baf941270bec55c3b7aea2333dc49982cbc969ef
ACFcoding <adrianc.franks@gmail.com>
2023-08-15 17:02:58 -0600

    Added boilerplate code and the initial constants

---
+++ index.js
@@ -0,0 +1,8 @@
+const playerTxt = document.querySelector("player");
+const opponentTxt = document.querySelector("opponent");
+const gameResultTxt = document.querySelector("result");
+const moveSelectTxt = document.querySelectorAll("moveSelect");
+
+let player;
+let opponent;
+let gameResult;
